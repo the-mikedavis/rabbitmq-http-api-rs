54189fa44d20b7ed484ff2e50ad7f88d0fc6af36
Michael Klishin <michael@clojurewerkz.org>
2023-05-25 03:23:43 +0400

    Tests for GET /api/users, GET /api/users/{name}

---
+++ tests/users_tests.rs
@@ -0,0 +1,27 @@
+use rabbitmq_http_client::{blocking::Client};
+
+mod common;
+use crate::common::{endpoint, USERNAME, PASSWORD};
+
+#[test]
+fn test_list_users() {
+    let endpoint = endpoint();
+    let rc = Client::new_with_basic_auth_credentials(&endpoint, USERNAME, Some(PASSWORD));
+    let result = rc.list_users();
+
+    assert!(result.is_ok());
+    let vec = result.unwrap();
+    assert!(vec.iter().find(|u| u.name == "guest").is_some())
+}
+
+#[test]
+fn test_get_user() {
+    let endpoint = endpoint();
+    let rc = Client::new_with_basic_auth_credentials(&endpoint, USERNAME, Some(PASSWORD));
+    let name = "guest";
+    let result = rc.get_user(name);
+
+    assert!(result.is_ok());
+    let u = result.unwrap();
+    assert!(u.name == name);
+}
