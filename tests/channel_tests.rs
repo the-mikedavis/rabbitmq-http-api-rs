60414cee50083845d92fc6c2da18fe98ecec79ca
Michael Klishin <michael@clojurewerkz.org>
2023-05-31 14:45:53 +0400

    One more test

---
+++ tests/channel_tests.rs
@@ -0,0 +1,13 @@
+use rabbitmq_http_client::blocking::Client;
+
+mod common;
+use crate::common::{endpoint, PASSWORD, USERNAME};
+
+#[test]
+fn test_list_channels() {
+    let endpoint = endpoint();
+    let rc = Client::new_with_basic_auth_credentials(&endpoint, USERNAME, Some(PASSWORD));
+
+    let result1 = rc.list_channels();
+    assert!(result1.is_ok());
+}
