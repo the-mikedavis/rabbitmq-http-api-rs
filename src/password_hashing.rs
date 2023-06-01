c101e9df12b62ded17863eab99c8d44af08ed845
Michal Kuratczyk <mkuratczyk@vmware.com>
2023-06-01 09:27:37 +0200

    Address clippy suggestions for hashing

--- src/password_hashing.rs
+++ src/password_hashing.rs
@@ -10,18 +10,18 @@ pub fn salt() -> Vec<u8> {
     Vec::from(bytes)
 }
 
-pub fn salted_password_hash_sha256(salt: &Vec<u8>, password: &str) -> Vec<u8> {
+pub fn salted_password_hash_sha256(salt: &[u8], password: &str) -> Vec<u8> {
     let mut ctx = Context::new(&SHA256);
-    let vec = Vec::from([&salt[..], &password.as_bytes()[..]].concat());
+    let vec = [salt, password.as_bytes()].concat();
 
     ctx.update(&vec);
     let digest = ctx.finish();
     let digest_vec = Vec::from(digest.as_ref());
 
-    [&salt[..], &digest_vec[..]].concat()
+    [salt, &digest_vec[..]].concat()
 }
 
-pub fn base64_encoded_salted_password_hash_sha256(salt: &Vec<u8>, password: &str) -> String {
+pub fn base64_encoded_salted_password_hash_sha256(salt: &[u8], password: &str) -> String {
     let salted = salted_password_hash_sha256(salt, password);
     rbase64::encode(salted.as_slice())
 }
