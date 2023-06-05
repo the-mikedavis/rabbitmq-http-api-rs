4be5f6d7ee669d38ae865a9c7859c0d94b01ee14
Michael Klishin <michael@clojurewerkz.org>
2023-06-05 08:15:06 +0400

    bin/ci/before_build.sh updates

--- bin/ci/before_build.sh
+++ bin/ci/before_build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-CTL=${RABBITHOLE_RABBITMQCTL:="sudo rabbitmqctl"}
-PLUGINS=${RABBITHOLE_RABBITMQ_PLUGINS:="sudo rabbitmq-plugins"}
+CTL=${RUST_HTTP_API_CLIENT_RABBITMQCTL:="sudo rabbitmqctl"}
+PLUGINS=${RUST_HTTP_API_CLIENT_RABBITMQ_PLUGINS:="sudo rabbitmq-plugins"}
 
 case $CTL in
         DOCKER*)
@@ -26,8 +26,8 @@ $CTL set_permissions -p / guest ".*" ".*" ".*"
 $CTL eval 'supervisor2:terminate_child(rabbit_mgmt_sup_sup, rabbit_mgmt_sup), application:set_env(rabbitmq_management,       sample_retention_policies, [{global, [{605, 1}]}, {basic, [{605, 1}]}, {detailed, [{10, 1}]}]), rabbit_mgmt_sup_sup:start_child().'
 $CTL eval 'supervisor2:terminate_child(rabbit_mgmt_agent_sup_sup, rabbit_mgmt_agent_sup), application:set_env(rabbitmq_management_agent, sample_retention_policies, [{global, [{605, 1}]}, {basic, [{605, 1}]}, {detailed, [{10, 1}]}]), rabbit_mgmt_agent_sup_sup:start_child().'
 
-$CTL add_vhost "rabbit/hole"
-$CTL set_permissions -p "rabbit/hole" guest ".*" ".*" ".*"
+$CTL add_vhost "rust/http/api/client"
+$CTL set_permissions -p "rust/http/api/client" guest ".*" ".*" ".*"
 
 # set cluster name
 $CTL set_cluster_name rabbitmq@localhost
@@ -42,5 +42,4 @@ $PLUGINS enable rabbitmq_shovel_management
 $PLUGINS enable rabbitmq_federation
 $PLUGINS enable rabbitmq_federation_management
 
-export GOMEGA_DEFAULT_EVENTUALLY_TIMEOUT="5s"
 true
