fa2dab42dd44ecccd378b4bfcdaffdd2478824f9
Michal Kuratczyk <mkuratczyk@vmware.com>
2023-06-06 11:20:46 +0200

    Print received value if assertion fails

--- tests/channel_tests.rs
+++ tests/channel_tests.rs
@@ -9,5 +9,5 @@ fn test_list_channels() {
     let rc = Client::new_with_basic_auth_credentials(&endpoint, USERNAME, PASSWORD);
 
     let result1 = rc.list_channels();
-    assert!(result1.is_ok());
+    assert!(result1.is_ok(), "list_channels returned {:?}", result1);
 }
